9e4e4586f9413ae5ce8dbcc016844d0b2cdce9ad
sonam dorji <02230200.cst@rub.edu.bt>
2023-10-06 17:23:05 +0600

    first upload of file

--- SWE_SONAMDORJIGHALLEY_02230299.py
+++ SWE_SONAMDORJIGHALLEY_02230299.py
@@ -2,17 +2,12 @@ import requests, time
 from selenium import webdriver                                              
 from selenium.webdriver.common.by import By
 
-# use selenium loop through all my peer's leetcode profile
-# get their last activity using xpath
-# check if it has been more than a dqay/24hours
-# if yes send message in telegram group that they did not dom leetcode
 
-# wrong username      
-# Chunku tamang 'Hleeseu'
 usernames = [
         'SDGV2734',
         'khemraj9815',
         'C-gyeltshen',
+        'ENHleeseu',
         'DwangShers',
         'Dupchuwangmo7',
         'Kanishapradhan13',
@@ -82,48 +77,12 @@ def send_telegram_message(message):
 for username in usernames:
     last_activity_and_send_message(username)
 
-# last_activity_and_send_message(usernames[2])
+
+
+
+ 
 
 
 
 
 
-
-
-
-'''import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-usernames = [
-    "SDGV2734",
-    "khemraj9815"
-]
-
-driver = webdriver.Chrome()
-
-def last_activity_and_send_message(username):
-    driver.get(f"https://leetcode.com/{username}")
-
-    last_activity_element = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div[3]/div/div/div[2]/a[1]/div/span[2]")
-    last_activity_text = last_activity_element.text
-
-    if "day" in last_activity_text:
-        days_ago = int(last_activity_text.split()[0])
-        if days_ago > 1:
-            send_telegram_message(f"{username} please do your LeetCode problems")
-
-    elif "hour" in last_activity_text:
-        hours_ago = int(last_activity_text.split()[0])
-        if hours_ago > 24:
-            send_telegram_message(f"{username} did not do LeetCode for more than 24 hours.")
-
-def send_telegram_message(message):
-    token = "6340249508:AAEEOqOv7Jt1VnLfAxrObQE1-0h-xdBUKEg"
-    chat_id = "@cstsweleetcode"
-    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
-    response = requests.get(url)
-    print(response.json())  # This sends the message
-
-for username in usernames:
-    last_activity_and_send_message(username)'''
